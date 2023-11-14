6fb808ec7b55e02bd6306a610f9561dcc3dc56a1
Felipe Franco <67972456+fe-franco@users.noreply.github.com>
2023-11-13 22:10:13 -0300

    Bigger font

--- src/app/prestador/[id]/page.jsx
+++ src/app/prestador/[id]/page.jsx
@@ -7,7 +7,7 @@ export default function Page({ params }) {
   return (
     <main className="max-w-7xl mx-auto">
       <div className="flex flex-col space-y-4 py-8">
-        <h1 className="text-3xl font-semibold leading-none tracking-tight">
+        <h1 className="text-4xl font-semibold leading-none tracking-tight">
           {id ? "Editar Prestador" : "Criar Prestador"}
         </h1>
         <h2 className="text-sm font-semibold leading-none tracking-tight text-muted-foreground">
